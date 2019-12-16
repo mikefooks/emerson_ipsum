a650be08b3a001db5236c5c3ff21e9af4ecd631e
Mike Fooks <mikefooks@gmail.com>
2019-12-16 12:27:13 -0800

    ability to create more complex paragraph structures added.

--- loremerson/__init__.py
+++ loremerson/__init__.py
@@ -12,6 +12,7 @@ api = Api(application)
 parser = reqparse.RequestParser()
 parser.add_argument('s', type=int, help='number of sentences')
 parser.add_argument('p', type=int, help='numbers of paragraphs')
+parser.add_argument('counts', type=str, help='number of sentences in each paragraph.')
 
 data_path = os.environ['LOREMERSON_DATA_PATH']
 sent_gen = SentenceGen(data_path)
@@ -22,6 +23,11 @@ class EmersonGuest(Resource):
         args = parser.parse_args()
 
         sentences, paras = args['s'], args['p']
+        sent_counts = args['counts']
+
+        if sent_counts:
+            sent_counts = tuple(map(int, sent_counts.split(',')))
+            return self._build_paragraphs(sent_counts)
         
         if sentences:
             if paras:
@@ -29,9 +35,7 @@ class EmersonGuest(Resource):
                          for _ in range(paras) ]
                 return { 'text': text }
             else:
-                return {
-                    'text': sent_gen(count=sentences)
-                }
+                return { 'text': sent_gen(count=sentences) }
         
         if paras:
             text = [ sent_gen(count=1)
@@ -43,7 +47,7 @@ class EmersonGuest(Resource):
         
     def _build_paragraphs (self, sent_counts):
         paragraphs = []
-        for n in sent_count_list:
+        for n in sent_counts:
             paragraphs.append(sent_gen(count=n))
 
         return { 'text': paragraphs }
