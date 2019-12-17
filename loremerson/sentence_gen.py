1dfefa0300e221856c7c8e46ce6c35762b12fb6e
Mike Fooks <mikefooks@gmail.com>
2019-12-17 09:34:04 -0800

    fixed the problem with the sentence gen counter overrunning.

--- loremerson/sentence_gen.py
+++ loremerson/sentence_gen.py
@@ -11,21 +11,24 @@ class SentenceGen:
         shuffle(self.sentences)
 
     def _get_sentence (self):
+        if self.idx >= self.length:
+            shuffle(self.sentences)
+            print(self)
+            self.idx = 0
+            return self._get_sentence()
         sentence = self.sentences[self.idx]
         self.idx += 1
         return sentence
 
+    def __repr__(self):
+        return '<SentenceGen length={},idx={}>'.format(self.length, self.idx)
+
     def __call__(self, count=1, as_list=False):
-        if self.idx >= self.length:
-            shuffle(self.sentences)
-            self.idx = 0
-            return self(*args, **kwargs)
+        sentences = [ self._get_sentence()
+                      for _ in range(count) ]
+        if as_list:
+            return sentences
         else:
-            sentences = [ self._get_sentence()
-                          for _ in range(count) ]
-            if as_list:
-                return sentences
-            else:
-                return " ".join(sentences)
+            return " ".join(sentences)
 
 
