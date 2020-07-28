827ce2aa62ee0abc63749beea1eeb15611f4b4ec
Mike Fooks <mikefooks@gmail.com>
2020-07-27 19:38:43 -0700

    working out the kinks with content negotiation

--- loremerson/__init__.py
+++ loremerson/__init__.py
@@ -1,20 +1,22 @@
+import sys
 import os
+import json
 
-from flask import Flask
+from flask import Flask, request, make_response
 from flask_restful import Resource, Api, reqparse
+from flask_restful.utils import unpack
 
 from loremerson.generators import StringGen
 
 
-data_path = os.environ["EMERSON_DATA_PATH"]
+DATA_PATH = os.environ["EMERSON_DATA_PATH"]
 
 # Instantiate the sentence and heading generators.
-sentence_gen = StringGen(os.path.join(data_path, "sentences.dat"))
-heading_gen = StringGen(os.path.join(data_path, "headings.dat"))
+sentence_gen = StringGen(os.path.join(DATA_PATH, "sentences.dat"))
+heading_gen = StringGen(os.path.join(DATA_PATH, "headings.dat"))
 
 # Initialize the application.
 application = Flask(__name__)
-api = Api(application)
 
 # Define query parameters.
 parser = reqparse.RequestParser()
@@ -24,25 +26,44 @@ parser.add_argument('paras',
 parser.add_argument('sentences',
                     type=int,
                     help='number of sentences')
-parser.add_argument('format',
-                    type=str,
-                    help='output format: one of json, html, text')
 parser.add_argument('headings',
                     type=int,
                     help='number of headings')
 
+api = Api(application)
+
+# Define different mimetype representations
+@api.representation('application/json')
+def json_response (data, code, headers):
+    resp = make_response(json.dumps(data), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+@api.representation('text/html')
+def html_response (data, code, headers):
+    data = [ '<p>' + p + '</p>' for p in data ]
+    data = '\n'.join(data['text'])
+    resp = make_response(data, code)
+    resp.headers.extend(headers or {})
+    return resp
+
+@api.representation('text/plain')
+def text_response (data, code, headers):
+    data = '\n'.join(data['text'])
+    resp = make_response(data, code)
+    resp.headers.extend(headers or {})
+    return resp
+
 # Define the resource.
-class EmersonGuest(Resource):
-    def get(self):
+@api.resource("/")
+class EmersonResource (Resource):
+    def get (self):
         args = parser.parse_args()
         paras = args['paras'] or 1
         sentences = args['sentences'] or 4
-        format = args['format'] or 'json'
+        headings = args['headings'] or 0
 
-        paragraphs = [ sentence_gen(count=sentences)
-                       for _ in range(paras) ]
-
-        return paragraphs
-
-
-api.add_resource(EmersonGuest, '/')
+        return {
+            'text': [ sentence_gen(count=sentences)
+                      for _ in range(paras) ]
+        }
